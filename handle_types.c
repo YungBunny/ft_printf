da6b449defef23f067dc41dc5b8e301e7a45cd77
cfu <cfu@e1z2r4p28.42.us.org>
2017-06-03 00:00:55 -0700

    need specifiers

--- handle_types.c
+++ handle_types.c
@@ -6,7 +6,7 @@
 /*   By: cfu <cfu@student.42.us.org>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/16 18:47:57 by cfu               #+#    #+#             */
-/*   Updated: 2017/06/01 01:59:57 by cfu              ###   ########.fr       */
+/*   Updated: 2017/06/02 23:57:57 by cfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,32 @@
 
 
 
-char		curr_type(char *str)
+char		curr_type(char **str)
 {
-	int			i;
 	struct pf	type;
 	int			total;
 
 	i = 0;
 	total = 0;
-	if (str)
+	if (*str)
 	{
-		structure_check();
+		structure_check(str, total, 0);
 		total = num_spec(str);
+		create_structs(str, total, 1);
 		/*function that checks structure*/
-		while (str[i])
+		while (**str)
 		{
-			if (str[i] == '%' )
+			if (**str == '%' )
 				/*send to function that creates number of structs needed*/
-				i++;
-			if (str[i] >= 49 && str[i] <= 57)
-				type.min_field_wid = ft_atoi(str[i]);
-			if (str[i] == 'd' || str[i] == 'i')
-				type_int(i);
-			if (str[i] == 'o' || str[i] == 'u' || str[i] == 'x' || str[i] == 'X')
-				type_digi(i);
-			if (str[i] == 'n')
+			{
+				flags_to_mod(&*str, type, 1);
+				**str++;
+			}
+			if (**str == 'd' || **str == 'i')
+				type_int(*str);
+			if (**str == 'o' || **str == 'u' || **str == 'x' || **str == 'X')
+				type_digi(*str);
+			if (**str == 'n')
 			/*passes in place in string, so you can check the spot before 
 			 * and make sure that the place before isn't negative*/
 		}
@@ -47,87 +48,236 @@ char		curr_type(char *str)
 
 int			num_spec(char *str)
 {
-	int		i;
 	int		count;
 
-	i = 0;
 	count = 0;
-	if (str)
+	if (*str)
 	{
-		while (str[i])
+		while (**str)
 		{
-			if (str[i] == '%' && str[i + 1] == '%')
+			if (**str == '%' && **(str + 1) == '%')
+			{
 				count++;
-			i++;
+				**str + 2;
+			}
+			else if (**str == '%')
+			{
+				count++;
+				*str++;
+			}
+			else
+				*str++;
 		}
 		return (count);
 	}
 }
 
-int			flags_to_mod(char *str, int i, struct fp type)
+int			flags_to_mod(char **str, struct fp type, int on)
 {
-	if (str[i] == '#' || str[i] == '0' || str[i] == '-' || str[i] == ' ' ||
-		str[i] == '+' || str[i] == ''')
+	if (**str == '#' || **str == '0' || **str == '-' || **str == ' ' ||
+		**str == '+' || **str == ''')
 	{
-		flag_check(str, i, type);
-		i++;
+		flag_check(&*str, type, on);
+		*str++;
 	}
-	if (str[i] >= 49 && str[i] <= 57)
+	if (**str >= 49 && **str <= 57)+
 	{
-		field_wid(str, i, type);
-		i++;
+		field_wid(&*str, type, on);
+		*str++;
 	}
-	if (str[i] =='.')
+	if (**str =='.')
 	{
-		i++;
-		if (str[i] >= 49 && str[i] <= 57)
-			precision(str, i, type);
-		i++;
+		*str++;
+		if (**str >= 49 && **str <= 57)
+			precision(&*str, type, on);
+		*str++;
 	}
+	to_mod(&*str, type, on);
 }
 
-int			to_mod(char *str, int i, struct fp type)
+int			flag_check(char *str, struct fp type, int on)
 {
-	int		n;
+	if (**str == '#' && on == 1)
+		type.pound = '#';
+	if (**str == '0' && on == 1)
+		type.zero = '0';
+	if (**str == '-' && on == 1)
+		type.dash = '-';
+	if (**str == ' ' && on == 1)
+		type.space = ' ';
+	if (**str == '+' && on == 1)
+		type.plus = '+';
+	if (**str == ''' && on == 1)
+		type.apost = ''';
+	return (1);
+}
 
-	n = i;
-	if (str[i] == 'h' || str[i] == 'l')
+int			to_mod(char **str, struct fp type, int on)
+{
+	if (**str == 'h')
 	{
-		n++;
-		if (str[n] == 'h')
-		{
-			type.modifier = ft_strdup("hh");
-			return (1);
-		}
-		if (str[n] == 'l')
-		{
-			type.modifier = ft_strdup("ll");
-			return (1);
-		}
+		check_h(&*str, type, on);
+		return (1);
 	}
-	if (str[i] == 'j' || str[i] == 't' || str[i] == 'z' || str[i] == 'q')
+	if (**str == 'l')
 	{
-		if (mods(str, i, type) == 1)
+		check_l(&*str, type, on);
+		return (1);
+	}
+	if (**str == 'j' || **str == 't' || **str == 'z' || **str == 'q')
+	{
+		if (mods_jtzq(&*str, type, on) == 1)
 			return (1);
 	}
 	return (0);
 }
 
-void		flag_check(char *str, int i, struct fp type)
+int			check_h(char **str, struct fp type, int on)
 {
-	if (str[i] == '#')
-		type.pound = '#';
-	if (str[i] == '0')
-		type.zero = '0';
-	if (str[i] == '-')
-		type.dash = '-';
-	if (str[i] == ' ')
-		type.space = ' ';
-	if (str[i] == '+')
-		type.plus = '+';
-	if (str[i] == ''')
-		type.apost = ''';
+	char	**cpy;
+
+	cpy = str;
+	if (**str == 'h')
+	{
+		**cpy++;
+		if (**cpy == 'h')
+		{
+			**str + 2;
+			if (on == 1)
+				type.modifier = '1';
+			return (1);
+		}
+		if (on == 1)
+			type.modifier = '2';
+		return (1);
+	}
+	return (0);
 }
+
+int			check_l(char **str, struct fp, type, int on)
+{
+	char	**cpy;
+
+	cpy = str;
+	if (**str == 'l')
+	{
+		**cpy++;
+		if (**cpy == 'l')
+		{
+			**str + 2;
+			if (on == 1)
+				type.modifier = '4';
+			return (1);
+		}
+		if (on == 1)
+			type.modifier = '3';
+		return (1);
+	}
+	return (0);
+}
+
+int			mods_jtzq(char **str, struct fp type, int on)
+{
+	if (on == 0)
+		return (1);
+	if (on == 1)
+	{
+		if (**str == 'j')
+			type.modifier = '5';
+		if (**str == 't')
+			type.modifier = '6';
+		if (**str == 'z')
+			type.modifier = '7';
+		if (**str == 'q')
+			type.modifier = '8';
+		return (1);
+	}
+	return (0);
+}
+
+void		field_wid(char **str, struct fp type, int on)
+{
+	int		size;
+	char	*convert;
+	char	**cpy;
+
+	size = 0;
+	convert = NULL;
+	cpy = str;
+	if (*str)
+	{
+		while (**cpy >= 49 && **cpy <= 57)
+			size++;
+		convert = ft_memalloc(sizeof(char *)* size);
+		while (**str >= 49 && **str <= 57)
+		{
+			*convert = **str;
+			convert++;
+			*str++;
+		}
+		wid_struct(convert, type, on);
+	}
+	return (0);
+}
+
+int			wid_struct(char *convert, struct fp type, int on)
+{
+	int		width;
+
+	width = ft_atoi(convert);
+	if (on == 1)
+	{
+		type.min_field_wid = width;
+		ft_strdel(width);
+		return (1);
+	}
+	if (on == 0)
+		return (1);
+	return (0);
+}
+
+void		precision(char **str, struct fp type, int on)
+{
+	int		size;
+	char	*convert;
+	char	**cpy;
+
+	size = 0;
+	convert = NULL;
+	cpy = str;
+	if (*str)
+	{
+		while (**cpy >= 49 && **cpy <= 57)
+			size++;
+		convert = ft_memalloc(sizeof(char *)* size);
+		while (**str >= 49 && **str <= 57)
+		{
+			*convert = **str;
+			convert++;
+			*str++;
+		}
+		pre_struct(convert, type, on);
+	}
+	return (0);
+}
+
+int			pre_struct(char *convert, struct fp type, int on)
+{
+	int		prec;
+
+	prec = ft_atoi(convert);
+	if (on == 1)
+	{
+		type.precision = prec;
+		ft_strdel(prec);
+		return (1);
+	}
+	if (on == 0)
+		return (1);
+	return (0);
+}
+
+
 
 void		structure_check(char *str)
 {
